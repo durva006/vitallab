ab6642aba8c110e7e978ed023a90e1890d59c287
Durva Deorukhkar <deorukhkardurva08@gmail.com>
2026-03-14 21:13:58 +0530

    Fixed nested folder structure

--- vitallab/src/main/java/com/vitallab/vitallab/repository/TestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.vitallab.vitallab.repository;
-
-import com.vitallab.vitallab.entity.Test;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestRepository extends JpaRepository<Test, Long> {
-}
